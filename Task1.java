d10754ddbbff5d5560b33f89702b84d522f7bd2b
Anastasia-2-0 <nosova0702@mail.ru>
2023-02-19 17:50:38 +0300

    решение задачи

--- Task1.java
+++ Task1.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество в последовательности(последнее число должно равняться нулю)");
         int num1 = scanner.nextInt();
+        System.out.println("Введите числа в последовательности");
         int num2 = scanner.nextInt();
-        int count = 0;
+        int sum = 0;
         while (num2 != 0) {
             int num3 = scanner.nextInt();
             if( num2 > 0 && num3 < 0) {
-                count++;
+                sum = sum + num2;
             }
             num2 = num3;
         }
-        System.out.println(count);
+        System.out.println(sum);
     }
 }
